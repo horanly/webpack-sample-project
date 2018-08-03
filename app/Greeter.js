c02eb91bae52eccd9c9ecb12566c121f0c42abb9
horan <horanrao@163.com>
2018-08-03 18:41:16 +0800

    babel

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,6 +1,15 @@
+//Greeter,js
+import React, {Component} from 'react'
+import config from './config.json';
 
-module.exports = function (){
-	var greet = document.createElement('div');
-	greet.textContent = "hello webpack";
-	return greet;
+class Greeter extends Component{
+  render() {
+    return (
+      <div>
+        {config.greetText}
+      </div>
+    );
+  }
 }
+
+export default Greeter
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,30 @@
-
 module.exports = {
-	devtool : "eval-source-map",     //生产阶段不建议启动
-	entry: __dirname + "/app/main.js",  //入口文件
-	output : {
+	devtool: "eval-source-map", //生产阶段不建议启动
+	entry: __dirname + "/app/main.js", //入口文件
+	output: {
 		path: __dirname + "/public", //打包文件存放位置
 		filename: "build.js" // 打包后输出的文件名字
 	},
-	
-	devServer : {
-		contentBase:"./public", //本地服务器所加载的页面所在的目录
+
+	devServer: {
+		contentBase: "./public", //本地服务器所加载的页面所在的目录
 		historyApiFallback: true, // 不跳转
-		inline:true, //实时刷新
+		inline: true, //实时刷新
 		port: "3033"
+	},
+
+	module: {
+		rules: [{
+			test: /(\.jsx|\.js)$/,
+			use: {
+				loader: "babel-loader",
+				options: {
+					presets: [
+						"env", "react"
+					]
+				}
+			},
+			exclude: /node_modules/
+		}]
 	}
 }
