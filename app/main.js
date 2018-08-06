fcae7b272b574d3f6a856301637cf15b69941531
horan <horanrao@163.com>
2018-08-06 16:25:53 +0800

    一切皆模块

--- app/main.js
+++ app/main.js
@@ -1,6 +1,9 @@
+//main.js
 
 import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Greeter';
+
+import './main.css'
 
 render (<Greeter />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
 	devtool: "eval-source-map", //生产阶段不建议启动
 	entry: __dirname + "/app/main.js", //入口文件
@@ -14,7 +16,8 @@ module.exports = {
 	},
 
 	module: {
-		rules: [{
+		rules: [
+		{
 			test: /(\.jsx|\.js)$/,
 			use: {
 				loader: "babel-loader",
@@ -25,6 +28,25 @@ module.exports = {
 				}
 			},
 			exclude: /node_modules/
-		}]
-	}
-}
+		},
+		{
+			test: /\.css$/,
+			use:[{
+				loader:"style-loader"
+			},{
+				loader: "css-loader",
+        options: {
+            modules: true // 指定启用css modules
+            //localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
+        }
+			},{
+				loader: "postcss-loader"
+			}
+			]
+		}
+		]
+	},
+	plugins:[
+		new webpack.BannerPlugin('版权所有，翻版必究')
+	],
+};
